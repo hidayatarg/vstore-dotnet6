ae56393ddf41b91b427e9b064575a7481f0538a1
Hidayat Arghandabi <hidayatarg@gmail.com>
2022-11-24 16:09:48 +0300

    Created Product and StoreContext

---
+++ vStore.API/Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using vStore.API.Entities;
+
+namespace vStore.API.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- vStore.API/Program.cs
+++ vStore.API/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using vStore.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// sqlserver connections
+builder.Services.AddDbContext<StoreContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
 
 var app = builder.Build();
 
