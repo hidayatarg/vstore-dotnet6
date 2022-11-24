e4ecab0d9f8a5e160ca0809a01ed5b630a6b3454
Hidayat Arghandabi <hidayatarg@gmail.com>
2022-11-24 22:11:05 +0300

    React useState

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,27 @@
-const products = [
-  { name: "product 1", price: 100.00 },
-  { name: "product 2", price: 100.00 }
-]
+import { useState } from 'react';
 
 function App() {
+  const [products, setProducts] = useState([
+    { name: "product 1", price: 100.00 },
+    { name: "product 2", price: 100.00 }
+  ]);
+  function addProduct() {
+    setProducts([...products, {name:"product 3", price: 300} ])
+  }
   return (
     <div>
       <h1>React vStore App</h1>
       <h3>Products</h3>
       <ul>
-        { products.map((product,i) => 
-          <li key={i} >{product.name} - {product.price} </li>) 
+        { products.map((product) => 
+          <li key={product.name} >{product.name} - {product.price} </li>) 
         }
       </ul>
+      <button onClick={addProduct}>Add Product</button>
     </div>
   );
 }
 
 export default App;
+
+
