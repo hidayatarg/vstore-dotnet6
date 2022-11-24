dcd6e36dd5e35993ea054ec89760053f44f133ed
Hidayat Arghandabi <hidayatarg@gmail.com>
2022-11-24 16:58:20 +0300

    Added ProductControlled

---
+++ vStore.API/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using vStore.API.Data;
+using vStore.API.Entities;
+
+namespace vStore.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly StoreContext _storeConext;
+
+        public ProductController(StoreContext storeConext)
+        {
+            _storeConext = storeConext;
+        }
+
+        [HttpGet]
+        public ActionResult<List<Product>> GetProducts()
+        {
+            var items =  _storeConext.Products.ToList();
+            return Ok(items);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+            if (id <= 0)
+                return BadRequest();
+
+            var item = _storeConext.Products.FirstOrDefault(p => p.Id == id);
+            
+            if (item == null)
+                return NotFound();
+            
+            return Ok(item);
+        }
+
+
+    }
+}
