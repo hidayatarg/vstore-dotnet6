08b2cf53f65354ebc4b6a0eb21b6bf96cd7f2d91
Hidayat Arghandabi <hidayatarg@gmail.com>
2022-11-24 22:00:04 +0300

    React Loop

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,18 @@
+const products = [
+  { name: "product 1", price: 100.00 },
+  { name: "product 2", price: 100.00 }
+]
 
 function App() {
   return (
     <div>
       <h1>React vStore App</h1>
+      <h3>Products</h3>
+      <ul>
+        { products.map((product,i) => 
+          <li key={i} >{product.name} - {product.price} </li>) 
+        }
+      </ul>
     </div>
   );
 }
