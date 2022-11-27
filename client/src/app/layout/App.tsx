7cfd9c2cdf53605cfd5cc33bc3fcc0993493f357
Hidayat Arghandabi <hidayatarg@gmail.com>
2022-11-27 17:36:37 +0300

    Implemented Black Theme Mode

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,18 +1,32 @@
-import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@emotion/react';
+import { createTheme, CssBaseline } from '@mui/material';
 import { Container } from '@mui/system';
 import Catalog from '../../features/catalog/Catalog';
 import Header from './Header';
+import { useState } from 'react';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? 'dark' : 'light';
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+    }
+  })
+
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
   
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Header />
+      <Header handleThemeChange={handleThemeChange} darkMode={darkMode}/>
       <Container>
         <Catalog />
       </Container>
-    </>
+    </ThemeProvider>
   );
 }
 
