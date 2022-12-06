d9bbd2ad81e6201284de3cf661a3af66c22e8e8f
Hidayat Arghandabi <hidayatarg@gmail.com>
2022-12-07 00:45:28 +0300

    Added Basket Controller

---
+++ vStore.API/Controllers/BasketController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using vStore.API.Data;
+using vStore.API.Entities;
+
+namespace vStore.API.Controllers
+{
+    
+    public class BasketController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public BasketController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Basket>> GetBasket()
+        {
+            var basket = await _context.Baskets
+                .Include(i => i.Items)
+                .ThenInclude(p => p.Product)
+                .FirstOrDefaultAsync(x => x.BuyerId == Request.Cookies["buyerId"]);
+
+            if (basket == null) return NotFound();
+
+            return basket;
+        }
+    }
+}
