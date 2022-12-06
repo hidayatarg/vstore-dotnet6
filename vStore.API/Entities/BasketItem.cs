e50d041fdd6c253cd5e16d46bae404ef538fb3db
Hidayat Arghandabi <hidayatarg@gmail.com>
2022-12-07 00:36:47 +0300

    Added Basket EF Relationship and Migration

--- vStore.API/Entities/BasketItem.cs
+++ vStore.API/Entities/BasketItem.cs
@@ -1,5 +1,8 @@
-﻿namespace vStore.API.Entities
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace vStore.API.Entities
 {
+    [Table("Basketitems")]
     public class BasketItem
     {
         public int Id { get; set; }
@@ -10,5 +13,12 @@
         // we will not see product info but id
         public int ProductId { get; set; }
         public Product Product { get; set; }
+        
+        public int BasketId { get; set; }
+        public Basket Basket { get; set; }
     }
 }
+
+
+// Reference
+//https://learn.microsoft.com/en-us/ef/core/modeling/relationships?tabs=fluent-api%2Cfluent-api-simple-key%2Csimple-key
---
+++ vStore.API/Migrations/20221206213449_AddedBasketEntity.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace vStore.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedBasketEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Baskets",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BuyerId = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Baskets", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Basketitems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    BasketId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Basketitems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Basketitems_Baskets_BasketId",
+                        column: x => x.BasketId,
+                        principalTable: "Baskets",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Basketitems_Products_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Products",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Basketitems_BasketId",
+                table: "Basketitems",
+                column: "BasketId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Basketitems_ProductId",
+                table: "Basketitems",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Basketitems");
+
+            migrationBuilder.DropTable(
+                name: "Baskets");
+        }
+    }
+}
