476124cace4262909fb7dec4b5aeb7d2d5009de1
Hidayat Arghandabi <hidayatarg@gmail.com>
2022-12-21 16:53:19 +0300

    [React] Added BasketPage Styling

--- client/src/features/basket/BasketPage.tsx
+++ client/src/features/basket/BasketPage.tsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material';
+import { Delete } from '@mui/icons-material';
+import { IconButton, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from '@mui/material';
 import { useState, useEffect } from 'react'
 import agent from '../../app/api/agent';
 import Loading from '../../app/layout/Loading';
@@ -17,11 +18,43 @@ export default function BasketPage() {
       .finally(() => setLoading(false))
   }, [])
 
-  if (loading) return <Loading  message="Loading basket..."/>
+  if (loading) return <Loading message="Loading basket..." />
 
   if (!basket) return <Typography variant='h3'>Your basket is empty</Typography>
 
   return (
-    <h3>BuyerId = {basket.buyerId}</h3>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Products</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Quantity</TableCell>
+            <TableCell align="right">Subtotal</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {basket.items.map((row) => (
+            <TableRow
+              key={row.productId}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">$ {(row.price / 100).toFixed(2)}</TableCell>
+              <TableCell align="right">{row.quantity}</TableCell>
+              <TableCell align="right">$ {(row.quantity * (row.price / 100)).toFixed(2)}</TableCell>
+              <TableCell align="right">
+                <IconButton color='error'>
+                  <Delete />
+                </IconButton>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   )
 }
