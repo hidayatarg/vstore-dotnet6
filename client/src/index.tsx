65d573dbf9b38fb1e123275c84f26673f6f1e598
Hidayat Arghandabi <hidayatarg@gmail.com>
2022-12-21 18:06:47 +0300

    [React] Added React Context

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,6 +5,7 @@ import App from './app/layout/App';
 import './app/layout/styles.css';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserHistory } from 'history';
+import { StoreProvider } from './app/context/StoreContext';
 
 export const history = createBrowserHistory();
 
@@ -15,7 +16,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
         <Router history={history}>
-          <App />
+          <StoreProvider>
+            <App />
+          </StoreProvider>
         </Router>
       </BrowserRouter>
   </React.StrictMode>
