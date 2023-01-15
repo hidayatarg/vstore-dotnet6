baa60d6fd16828cda109102b89afb7cce0ad6c06
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-01-15 23:55:04 +0300

    [React] Product Details Component Add Items To Cart Functionality

--- client/src/features/catalog/ProductDetails.tsx
+++ client/src/features/catalog/ProductDetails.tsx
@@ -1,22 +1,32 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import { Product } from '../../app/models/product';
-import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, Typography } from '@mui/material';
+import { Divider, Grid, Table, TableBody, TableCell, TableContainer, TableRow, TextField, Typography } from '@mui/material';
 import agent from '../../app/api/agent';
 import Loading from '../../app/layout/Loading';
+import { useStoreContext } from '../../app/context/StoreContext';
+import { LoadingButton } from '@mui/lab';
 
 export default function ProductDetails() {
+  const {basket} = useStoreContext();
   const { id } = useParams<{ id: string }>();
   const [product, setProduct] = useState<Product | null>(null);
   const [loading, setLoading] = useState(true);
+  const [quantity, setQuantity] = useState(0);
+  const [submitting, setSubmitting] = useState(false);
+
+  // find item from basket
+  const item = basket?.items.find(i => i.productId === product?.id);
 
   useEffect(() => {
+    if (item) setQuantity(item.quantity);
+
     agent.Catalog.details(parseInt(id))
       .then(product => setProduct(product))
       .catch(err => console.log(err))
       .finally(() => setLoading(false));
 
-  }, [id])
+  }, [id, item])
 
   if (loading) return <Loading  message="Loading product..."/>
 
@@ -57,6 +67,28 @@ export default function ProductDetails() {
             </TableBody>
           </Table>
         </TableContainer>
+        <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <TextField 
+                  variant='outlined'
+                  type='number'
+                  label='Quantity in Cart'
+                  fullWidth
+                  value={quantity}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <LoadingButton
+                  sx={{height: '55px'}}
+                  color='primary'
+                  size='large'
+                  variant='contained'
+                  fullWidth
+              >
+                  {item ? 'Update Quantity' : 'Add to Cart'}
+              </LoadingButton>
+            </Grid>
+        </Grid>
       </Grid>
     </Grid>
   )
