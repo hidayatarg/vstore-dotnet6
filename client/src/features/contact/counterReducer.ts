def168a0786b0542d0306f0e5b42998b9b690339
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-01-16 02:27:16 +0300

    [React] ActionCreators

--- client/src/features/contact/counterReducer.ts
+++ client/src/features/contact/counterReducer.ts
@@ -2,8 +2,8 @@ export const INCREMENT_COUNTER = "INCREMENT_COUNTER";
 export const DECREMENT_COUNTER = "DECREMENT_COUNTER";
 
 export interface CounterState {
-    data: number,
-    title: string
+    data: number;
+    title: string;
 }
 
 const initalState: CounterState = {
@@ -11,17 +11,32 @@ const initalState: CounterState = {
     title: 'YARC'
 }
 
+// action Creator
+export function increment(amount = 1 ) {
+    return {
+        type: INCREMENT_COUNTER,
+        payload: amount
+    }
+}
+
+export function decrement(amount = 1 ) {
+    return {
+        type: DECREMENT_COUNTER,
+        payload: amount
+    }
+}
+
 export default function counterReducer(state = initalState, action: any) {
     switch (action.type) {
         case INCREMENT_COUNTER:
            return {
                 ...state,
-                data: state.data + 1
+                data: state.data + action.payload
            }
         case DECREMENT_COUNTER:
             return {
                 ...state,
-                data: state.data - 1
+                data: state.data - action.payload
             }
         default:
             return state;
