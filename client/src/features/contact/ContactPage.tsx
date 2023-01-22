5ca6c53e320a36b0b8cc281ad95dccfc09efb23d
Hidayat Arghandabi <hidayatarg@gmail.com>
2023-01-22 23:16:17 +0300

    [React] Component Integrated to Redux Store

--- client/src/features/contact/ContactPage.tsx
+++ client/src/features/contact/ContactPage.tsx
@@ -1,9 +1,18 @@
 import { Typography } from "@mui/material";
+import { useSelector } from "react-redux";
+import { CounterState } from "../checkout/counterReducer";
 
 export default function ContactPage() {
+  const {data, title} = useSelector((state: CounterState) => state);
   return (
-    <Typography variant="h2">
-        Contact Page
-    </Typography>
+    <>
+      <Typography variant="h2">
+          {title}
+      </Typography>
+
+      <Typography variant="h5">
+          The data:  {data}
+      </Typography>
+    </>
   )
 }
